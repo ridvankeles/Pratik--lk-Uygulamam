7d2522eeaf24ae535a3de64bc63c3e5f1eff807b
RIDVAN KELEŞ <84584401+ridvankeles@users.noreply.github.com>
2025-06-19 00:13:07 +0300

    Create .cs

---
+++ .cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rıdvan KELEŞ " +
+                "Patikada olma sebebim yeni bir kariyer planlamak ve yazılım dünyasına duyduğum merak. ");
+        }
+    }
+}
